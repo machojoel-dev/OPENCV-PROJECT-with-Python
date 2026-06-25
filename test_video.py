7c35f91fdbaa01a972db89d1ce8dfdfc0399b6b2
Macho joel <machojoel377@gmail.com>
2026-06-25 23:07:06 +0300

    added clear videos and fixed video code logic

--- test_video.py
+++ test_video.py
@@ -1,10 +1,17 @@
 import cv2
 
-cap = cv2.VideoCapture("videos/fish.mp4")
+cap = cv2.VideoCapture("videos/street.mp4")
 
 while True:
-    success, img = cap.read()
-    cv2.imshow("Video", img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    ret, frame = cap.read()
+
+    if not ret:
         break
-    
+
+    cv2.imshow("Video", frame)
+
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
